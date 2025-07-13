9f1efee5538c98bfd1949bb1f908d1554d71c275
JeffSSC <jeff.silva.caires@gmail.com>
2025-07-13 19:48:39 -0300

    fix(custom.d.ts): criado arquivo para declarar modulos e corrigir erro de importação de imagens e css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import Navbar from './components/navbar/navbar';
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './navbar.module.css';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-        <p className={styles.name}>JeffSC</p>
+        <p className={styles.logo}>JeffSC</p>
         <div>
         <Link to="/" className={styles.navLinks}>Home</Link>
         <Link to="/about" className={styles.navLinks}>Fotografias</Link>
