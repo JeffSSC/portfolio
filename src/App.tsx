fe5f85d2fea332559e95afdaf0d38fc2b92b5d7a
JeffSSC <jeff.silva.caires@gmail.com>
2025-07-14 10:02:55 -0300

    feat(footer):adicionado footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/Footer';
+
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <p>© 2025 Jefferson Silva Caires.</p>
+    </footer>
+  );
+};
+
+export default Footer;
